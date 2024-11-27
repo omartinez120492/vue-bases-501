16d77444c932d7f18b0555c89ab27d99076c2ac3
omartinez120492 <omar1204@gmail.com>
2024-11-27 11:38:27 -0600

    dsd

--- src/posts/PostApi.vue
+++ src/posts/PostApi.vue
@@ -3,19 +3,49 @@ import { ref } from 'vue';
 import PostCard from './components/PostCard.vue';
 
 const data = ref([])
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((resp) => resp.json())
-    .then((d) => data.value = d)
-    .finally(() => console.log('End api'))
 
+const getPosts = async () => {
+    try {
+        const resp = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const posts = await resp.json();
+        data.value = posts
+    } catch (error) {
+        console.log(error);
+    } finally {
+
+    }
+}
+
+const valor = 10;
+const inicio = ref(0)
+const fin = ref(10)
+
+const nextPosts = () => {
+    inicio.value = inicio.value + valor;
+    fin.value = fin.value + valor;
+}
+const backPosts = () => {
+    inicio.value = inicio.value - valor;
+    fin.value = fin.value - valor;
+}
+
+getPosts()
 </script>
 
 <template>
     <h5 class="display-5 text-primary">Home Page of Post</h5>
-    <button class="btn btn-primary me-3">Back</button>
-    <button class="btn btn-primary">Next</button>
+
+    <button class="btn btn-primary me-3" @click="backPosts">Back</button>
+    <button 
+        class="btn btn-primary" 
+        v-on:click="nextPosts"
+        :disabled="false"
+    >
+        Next
+    </button>
+
     <hr>
 
-    <PostCard v-for="p in data" :key="p.id" :id="p.id" :title="p.title" :body="p.body">
+    <PostCard v-for="p in data.slice(inicio, fin)" :key="p.id" :id="p.id" :title="p.title" :body="p.body">
     </PostCard>
 </template>
